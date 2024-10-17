781fb68c53ea282ae2cd5b9c8b80b2cf88eeab7d
Edilson Junior <etmjr03@gmail.com>
2024-10-17 00:09:16 -0300

    Adiciona recursos de propostas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         Project::factory(10)->create();
+        Proposal::factory(10)->create(['project_id' => Project::all()->random()->id]);
     }
 }
